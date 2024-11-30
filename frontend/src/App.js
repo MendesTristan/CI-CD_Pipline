45625de78df098affb11fdea202944c0bdc514b9
MendesTristan <167921344+MendesTristan@users.noreply.github.com>
2024-11-30 14:11:50 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,7 @@ import Garden from './components/Garden';
 import TaskInput from './components/TaskInput';
 import axios from 'axios';
 import Plant from './components/Plant';
-// commit
-//deux commit
+// 
 
 function App() {
   const [tasks, setTasks] = useState([]);
